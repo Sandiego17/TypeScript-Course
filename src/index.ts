6e48c977f1469571391dca6c4bab1131c66e387c
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2025-01-10 16:43:31 +0100

    update: TypeScript with Arrays and Object literals

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,47 @@ var isFictional: boolean = true;
 
 let something: null;
 let anotherThing: undefined;
+// ARRAYS
+let names: string[] = ['Henry', 'Tochukwu', 'Uzor'];
+let ages: number[] = [25, 28, 24];
+let bools: boolean[] = [true, false];
+
+names.push('Anuoluwapo', 'Ekomobong');
+ages.push(35);
+bools.pop();
+console.log(names, ages, bools);
+
+// type inference with ARRAYS
+let fruits = ['apple', 'pears', 'bananas', 'mangos'];
+let things = [1, true, 'hello'];
+
+fruits.push('peaches');
+const f = fruits[3];
+
+console.log(fruits);
+console.log('Variable "f" is of datatype:', typeof(f), 'and value of:', f);
+console.log(typeof(things));
+
+// OBJECT LITERALS
+let user: {id: number, firstName: string, age: number} = {
+  id: 1,
+  firstName: 'Mario',
+  age: 32
+};
+
+user.id = 2;
+user.firstName = 'Pikanto';
+
+console.log(user);
+
+// type inference with OBJECT LITERALS
+let person = {
+  name: 'Luigi',
+  score: 35
+};
+
+person.name = 'Votron';
+person.score = 40;
+
+console.log(person);
 
