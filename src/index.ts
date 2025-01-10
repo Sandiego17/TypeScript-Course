32923e21d1f04024e01fc514cb0b574f3692e7e6
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2025-01-10 16:41:06 +0100

    feat: TypeScript basics

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+var myAge: number = 35;
+
+console.log(myAge);
+
+var age: number = 35;
+var firstName: string = "Henry";
+var isFictional: boolean = true;
+
+let something: null;
+let anotherThing: undefined;
+
