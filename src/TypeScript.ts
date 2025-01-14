fbc903113840f2ea48136c353171355677ed8705
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2025-01-14 11:31:11 +0100

    update: TypeScript.ts to reflect type assertion

--- src/TypeScript.ts
+++ src/TypeScript.ts
@@ -4,8 +4,9 @@ function add(x: number, y: number): number {
   return x + y
 };
 
-
+// --------------
 // Benefits of TypeScript
+// --------------
 
 // 1. better error feedback
 function reverse (str: string) {
@@ -26,3 +27,11 @@ interface MenuItem {
 function printMenuItem (item: MenuItem) {
   console.log(item.title, ':', item.cost)
 }
+
+// --------------
+// Type Assertions
+// --------------
+let someValue: unknown = "this is a string";
+let strLength: number = (someValue as string).length;
+// OR
+let strsLength: number = (<string>someValue).length;
